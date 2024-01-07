e1d87389644c403037db75bd782d398275d6e0f6
Robin <robin.espinozar@gmail.com>
2024-01-07 15:03:20 -0500

    feat: Add New Category Item View

--- app/src/main/java/com/raerossi/notekeeper/ui/features/categorylist/CategoryItem.kt
+++ app/src/main/java/com/raerossi/notekeeper/ui/features/categorylist/CategoryItem.kt
@@ -1,15 +1,16 @@
 package com.raerossi.notekeeper.ui.features.categorylist
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -18,46 +19,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.raerossi.notekeeper.R
 import com.raerossi.notekeeper.domain.Category
-import com.raerossi.notekeeper.ui.features.utils.ProgressTopBar
 import com.raerossi.notekeeper.ui.features.utils.VerticalSpacer
 import com.raerossi.notekeeper.ui.features.utils.categoryShadow
 import com.raerossi.notekeeper.ui.theme.NoteKeeperTheme
-import com.raerossi.notekeeper.ui.theme.neutralVariant40
+import com.raerossi.notekeeper.ui.theme.neutralVariant90
 import com.raerossi.notekeeper.ui.theme.neutralVariant95
-import com.raerossi.notekeeper.ui.theme.primary50
 import com.raerossi.notekeeper.ui.theme.primary70
-import com.raerossi.notekeeper.ui.theme.title
 
 @Composable
-fun CategoryItem(category: Category? = null) {
+fun CategoryItem(category: Category? = null, height: Int = 125, width: Int? = null) {
+    val modifier = if (width == null) Modifier.fillMaxWidth() else Modifier.width(width.dp)
+
     Card(
-        modifier = Modifier
+        modifier = modifier
             .categoryShadow()
-            .height(125.dp)
-            .width(203.dp),
+            .height(height.dp),
         shape = MaterialTheme.shapes.medium
     ) {
-        CardContent(category)
+        CardContent(category, height)
     }
 }
 
 @Composable
-private fun CardContent(category: Category?) {
+private fun CardContent(category: Category?, height: Int) {
     Column() {
-        CategoryInformation(category)
-        CategoryAdvance(category)
+        CategoryInformation(category, (height * 0.6).dp)
+        CategoryAdvance(category, (height * 0.4).dp)
     }
 }
 
 @Composable
-private fun CategoryInformation(category: Category?) {
+private fun CategoryInformation(category: Category?, height: Dp) {
     Box(
         Modifier
             .fillMaxWidth()
-            .height(75.dp)
+            .height(height)
             .background(MaterialTheme.colorScheme.primary70)
     ) {
         Column(
@@ -83,11 +86,11 @@ private fun CategoryInformation(category: Category?) {
 }
 
 @Composable
-private fun CategoryAdvance(category: Category?) {
+private fun CategoryAdvance(category: Category?, height: Dp) {
     Box(
         Modifier
             .fillMaxWidth()
-            .height(50.dp)
+            .height(height)
             .background(Color.White)
     ) {
         Column(
@@ -97,7 +100,7 @@ private fun CategoryAdvance(category: Category?) {
             Text(
                 modifier = Modifier.padding(start = 18.dp),
                 text = "60%",
-                color = MaterialTheme.colorScheme.neutralVariant40,
+                color = Color(0xB35B6158),
                 style = MaterialTheme.typography.bodyLarge
             )
             VerticalSpacer(4)
@@ -129,6 +132,44 @@ private fun CategoryProgress(modifier: Modifier = Modifier, progress: Float) {
     }
 }
 
+
+@Composable
+fun AddCategoryItem(modifier: Modifier = Modifier, size: Int = 125) {
+    Card(
+        modifier = modifier
+            .categoryShadow()
+            .height(size.dp)
+            .width(size.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.neutralVariant90)
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.add_new_category),
+                    contentDescription = "Add New Category",
+                    tint = Color(0xFF0C212C)
+                )
+                VerticalSpacer(16)
+                Text(
+                    text = "Add New \n" + "Category",
+                    textAlign = TextAlign.Center,
+                    color = Color(0xFF0C212C),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CategoryItemPreview() {
@@ -137,6 +178,12 @@ fun CategoryItemPreview() {
             Modifier
                 .fillMaxSize()
                 .padding(top = 16.dp, start = 16.dp, end = 16.dp)
-        ) { CategoryItem() }
+        ) {
+            Column {
+                CategoryItem(width = 203)
+                VerticalSpacer(32)
+                AddCategoryItem()
+            }
+        }
     }
 }
--- app/src/main/java/com/raerossi/notekeeper/ui/features/categorylist/CategoryList.kt
+++ app/src/main/java/com/raerossi/notekeeper/ui/features/categorylist/CategoryList.kt
@@ -6,26 +6,26 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.raerossi.notekeeper.domain.Category
 import com.raerossi.notekeeper.domain.Task
 
 @Composable
-fun CategoryList(modifier: Modifier = Modifier, categoryList: List<Category>) {
+fun CategoryList(
+    modifier: Modifier = Modifier,
+    widthItem: Int? = null,
+    categoryList: List<Category>
+) {
     LazyRow(
         modifier = modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(categoryList) { category ->
-            CategoryItem(category)
+            CategoryItem(category = category, width = widthItem)
         }
         item {
-            AddCategory()
+            AddCategoryItem()
         }
     }
-}
-
-@Composable
-fun AddCategory(){
-
 }
